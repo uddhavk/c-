de6eb3476b53abd8dde1a0fb6b3d8a12042460ef
uddhavk <164346647+uddhavk@users.noreply.github.com>
2024-07-05 22:04:57 +0530

    Add files via upload

---
+++ ternary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    int b=5;
+
+    int largest = a>=b ? a : b; 
+    cout<<"largest value :"<<largest<<endl;
+
+  return 0;
+}
