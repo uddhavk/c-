221a9dc05e6d36b91bd6a8bfb1ac298dcf430b26
uddhavk <164346647+uddhavk@users.noreply.github.com>
2024-07-22 21:37:15 +0530

    Add files via upload

---
+++ parameter.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b=1){
+   int sum = a + b;
+    return sum;
+}
+
+int diff(int a, int b){
+    int ok = a - b;
+    return ok;
+}
+
+int main(){
+
+    int s = sum(99); // b=1 therefore b value is not given, if we want to give we can give value for b
+    cout<<"sum = "<<s<<endl;
+
+    int d = diff(6,3);
+    cout<<"diff = "<<d<<endl;
+
+    return 0;
+}
